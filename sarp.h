543afeb744bec2dc3c453c75b18c3b801165297a
juggernaut15 <51027488+juggernaut15@users.noreply.github.com>
2019-08-15 04:52:27 +0900

    Update sarp.h

--- sarp.h
+++ sarp.h
@@ -5,4 +5,21 @@ int broadcasting();
 void myInfo(char* itf,u_char *mac,uint8_t *ip);
 void atoiArgv(char *ip,uint8_t* rip);
 
+void print_start()
+{
+    printf("  ########## ########## ######### #########\n");
+    printf("  #          #        # #       # #       #\n");
+    printf("  #          #  *  *  # #       # #       #\n");
+    printf("  #          #   __   # #      #  #       #\n");
+    printf("  #          #        # #     #   #       #\n");
+    printf("  ########## ########## ######    #########\n");
+    printf("           # #        # #     #   #\n");
+    printf("           # #        # #      #  #\n");
+    printf("           # #        # #       # #\n");
+    printf("           # #        # #       # #\n");
+    printf("           # #        # #       # #s\n");
+    printf("  ########## #        # #       # #\n");
+    printf("\n");
+    printf("# 2 0 1 9 . 0 8. 1 5. H a c k a t h o n #\n");
+}
 
