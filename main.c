c29811f2d6fe13b1928d702168c0c66c3de59d94
khj0209 <gaegom0209@gmail.com>
2019-08-15 04:01:17 +0900

    Update main.c

--- main.c
+++ main.c
@@ -1,6 +1,25 @@
-#include <stdio.h>
+#include "sarp.h"
 
-int main() {
-    printf("Hello, World!\n");
+int main(int argc,char* argv[]){
+
+    if(argc!=2){
+        printf("Need more argument");
+        return -1;
+    }
+
+    u_char mMac[6];
+    uint8_t mIp[4];
+    
+    myInfo(argv[1], mMac, mIp);
+    
+    while(1){
+        int flag = 0;
+        flag = table_check();
+        if(flag!=0){
+            system("sudo arp -d wlan0");//, argv[1]);
+            broadcasting();
+        }
+    }
+
     return 0;
 }
